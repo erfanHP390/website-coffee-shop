7de1c3fcae82f9060bc8e74d14cc99d91e58c3ba
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-19 21:28:58 +0330

    import component navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
+import Navbar from "@/components/modules/Navbar/Navbar";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Navbar />
+    <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import React from 'react'
 function index() {
   return (
     <h1>
-      Home Coffee Shop
-      سلام من عرفان حسینی در پروژه نکست کافی هستم
     </h1>
   )
 }
