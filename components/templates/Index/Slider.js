69c4875179f61ed6d339003040d0a9d83439df99
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-21 21:31:52 +0330

    change color title heading

--- components/templates/Index/Slider.js
+++ components/templates/Index/Slider.js
@@ -31,7 +31,7 @@ function Slider() {
     <source src="/images/film/film-1.mp4" type="video/mp4" />
   </video>
   <div className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
-    <h2 className="text-primary font-weight-medium m-0  font_vazir_medium ">
+    <h2 className={`text-primary font-weight-medium m-0  font_vazir_medium  ${styles.h2_heading}`}>
       برای شما بهترین ها را به ارمغان آوردیم
     </h2>
     <h1 className="display-1 text-white m-0  font_vazir_medium ">در کنار شماییم</h1>
@@ -49,7 +49,7 @@ function Slider() {
     <source src="/images/film/film-5.mp4" type="video/mp4" />
   </video>
   <div className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
-    <h2 className="text-primary font-weight-medium m-0 font_vazir_medium">
+    <h2 className={`text-primary font-weight-medium m-0  font_vazir_medium  ${styles.h2_heading}`}>
       بهترین قهوه از دل جنگل های اتیوپی
     </h2>
     <h1 className="display-1 text-white m-0 font_vazir_medium">از آفریقا تا ایران</h1>
@@ -66,7 +66,7 @@ function Slider() {
     <source src="/images/film/film-2.mp4" type="video/mp4" />
   </video>
   <div className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
-    <h2 className="text-primary font-weight-medium m-0 font_vazir_medium">
+    <h2 className={`text-primary font-weight-medium m-0  font_vazir_medium  ${styles.h2_heading}`}>
       8 سال بهترین در مشتری مداری
     </h2>
     <h1 className="display-1 text-white m-0 font_vazir_medium">در کنار خاطرات شما</h1>
@@ -79,7 +79,7 @@ function Slider() {
         <div
           class={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}
         >
-          <h2 class="text-primary font-weight-medium m-0 font_vazir_medium">
+          <h2 className={`text-primary font-weight-medium m-0  font_vazir_medium  ${styles.h2_heading}`}>
             محیطی آرامش بخش
           </h2>
           <h1 class="display-1 text-white m-0 font_vazir_medium">در کافه آرامش</h1>
