9908340a163031d19a7559384b783923dce1ad31
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-21 23:51:29 +0330

    import compo-tem

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Slider from '@/components/templates/Index/Slider'
 import About from '@/components/templates/Index/About'
+import Services from '@/components/templates/Index/Services'
 
 function index() {
   return (
     <>
     <Slider />
     <About/>
+    <Services />
     </>
   )
 }
