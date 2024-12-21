b535e9458f033d5e8b88f42e42a6ad25a6e201d9
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-21 23:51:16 +0330

    create module component service-item

--- components/modules/ServiceItem/ServiceItem.js
+++ components/modules/ServiceItem/ServiceItem.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export default function ServiceItem() {
+export default function ServiceItem({ title, desc, img, icon }) {
   return (
-    <div>
-      
+    <div class="col-lg-6 mb-5">
+      <div class="row align-items-center">
+        <div class="col-sm-5">
+          <img class="img-fluid mb-3 mb-sm-0" src={img} alt="" />
+        </div>
+        <div class="col-sm-7">
+          <h4 className='font_vazir_light'>
+            <i class="fa fa-truck service-icon  "></i>
+            {title}
+          </h4>
+          <p class="m-0  font_vazir_regular">{desc}</p>
+        </div>
+      </div>
     </div>
   )
 }
