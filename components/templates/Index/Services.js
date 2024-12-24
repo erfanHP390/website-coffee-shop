d50cbcaa86536caef14502fd1c0ebc3c222ce5a9
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-24 22:31:45 +0330

    map for services from db

--- components/templates/Index/Services.js
+++ components/templates/Index/Services.js
@@ -4,7 +4,7 @@ import ServiceItem from '@/components/modules/ServiceItem/ServiceItem'
 
 import styles from "../../../styles/About.module.css"
 
-function Services() {
+function Services({services}) {
   return (
     <div class="container-fluid pt-5">
       <div class="container">
@@ -18,30 +18,16 @@ function Services() {
           <h1 class={`${styles.display_4} font_vazir_extra_bold`}>مواد تازه و ارگانیک</h1>
         </div>
         <div class="row">
-          <ServiceItem
-            title="تماس از شما ، رساندن از ما!"
-            desc="با تنها یک تماس، سفارش یک فنجان قهوه‌ی خاص را به سرعت ثبت کنید و لحظات تازه و شادی را تجربه کنید"
-            img="/images/service-1.jpg"
-            icon="faTruck"
+          {
+            services.map(service => (
+              <ServiceItem
+            title={service.title}
+            desc={service.desc}
+            img={service.img}
+            icon={service.icon}
           />
-          <ServiceItem
-            title="دانه های قهوه تازه"
-            desc="تذوق لحظه‌ی منحصر به فردی را تجربه کنید با دانه های قهوه‌ی تازه و بی‌نظیر ما!"
-            img="/images/service-2.jpg"
-            icon="faCoffee"
-          />
-          <ServiceItem
-            title="رزرو آنلاین"
-            desc="با رزرو سریع ، آنلاین ، ساده خاطرات خود را  ماندگار کنید."
-            img="/images/service-3.jpg"
-            icon="faTable"
-          />
-          <ServiceItem
-            title="بهترین در کیفیت"
-            desc="به دنیایی از عطر و طعم‌های خوش بو و لذت بخش خوش آمدید، جایی که قهوه‌ی تازه و با کیفیت در انتظار شماست"
-            img="/images/service-4.jpg"
-            icon="faAward"
-          />
+            ))
+          }
         </div>
       </div>
     </div>
