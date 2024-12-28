25aa3ba3497239ca5958b97a8f8269507fb57182
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-28 16:34:50 +0330

    add email and post to data-base

--- components/templates/Index/Offer.js
+++ components/templates/Index/Offer.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import swal from 'sweetalert';
 
 import styles from "../../../styles/Offer.module.css"
 
 function Offer() {
+
+  const [email , setEmail] = useState(null)
+
+  const addEmail = async (event) => {
+    event.preventDefault()
+
+    const response = await fetch("http://localhost:4000/newsLetters", {
+      method: "POST",
+      headers: {
+        "content-type" : "application/json"
+      },
+      body: JSON.stringify({email})
+    })
+
+    if(response.status === 201) {
+      setEmail("")
+      swal({
+        title: "ایمیل شما با موفقیت ثبت شد",
+        icon: "success",
+        buttons: "باشه"
+      })
+    }
+
+
+  }
+
   return (
     <div class={`${styles.offer} container-fluid my-5 py-5 text-center position-relative overlay-top overlay-bottom`}>
       <div class="container py-5">
@@ -13,15 +40,17 @@ function Offer() {
         </h4>
         <form class={`${styles.form_inline} justify-content-center mb-4`}>
           <div class={`${styles.input_group}`}>
-            <input
-              type="text"
-              class={`${styles.form_control} p-4  m-0  font_vazir_regular`}
-              placeholder="لطفا ایمیل خود را وارد کنید"
-              style={{ height: "60px" }}
 
+            <input type="text" 
+            class={`${styles.form_control} p-4  m-0  font_vazir_regular`}
+            placeholder="لطفا ایمیل خود را وارد کنید"
+            style={{ height: "60px" }}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}     
             />
             <div class={`${styles.input_group_append}`}>
               <button
+              onClick={addEmail}
                 class={`${styles.btn}  ${styles.btn_primary}  m-0  font_vazir_regular  font-weight-bold px-4`}
                 type="submit"
               >
