778067b39a0eb664d9377b9134ba29ba51a5def6
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-29 23:43:28 +0330

    import tem-compo reservation

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import About from '@/components/templates/Index/About'
 import Services from '@/components/templates/Index/Services'
 import Offer from '@/components/templates/Index/Offer'
 import Menu from '@/components/templates/Index/Menu'
+import Reservation from '@/components/templates/Index/Reservation'
 
 export default function index({data}) {
   return (
@@ -14,6 +15,7 @@ export default function index({data}) {
     <Services  services={data.services} />
     <Offer />
     <Menu data={data.menu} />
+    <Reservation />
     </>
   )
 }
