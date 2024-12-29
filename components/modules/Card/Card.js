c6252b26b384831deb9219f79684a702b2f2f032
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-29 21:53:46 +0330

    add T

--- components/modules/Card/Card.js
+++ components/modules/Card/Card.js
@@ -7,7 +7,7 @@ export default function Card({ title, desc, price, img }) {
     <div class="row align-items-center mb-5">
       <div class="col-4 col-sm-3">
         <img class={styles.img_menu} src={img} alt="" />
-        <h5 class={`${styles.menu_price}  font_vazir_Medium`}>{price}</h5>
+        <h5 class={`${styles.menu_price}  font_vazir_Medium`}>{price}T</h5>
       </div>
       <div class="col-8 col-sm-9  ">
         <h4 className="font_vazir_Medium">{title}</h4>
