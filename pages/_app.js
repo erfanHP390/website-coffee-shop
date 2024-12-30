8a6aab6dcba77376858e5b050a34abc6b8e548a2
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-30 16:55:38 +0330

    import mod-compo footer

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 import Navbar from "@/components/modules/Navbar/Navbar";
+import Footer from "@/components/modules/Footer/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
     <Navbar />
     <Component {...pageProps} />
+    <Footer />
     </>
   );
 }
