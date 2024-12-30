8eaee9fefae7088843334286e255fa6e9012460b
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-30 19:19:53 +0330

    update styles

--- components/modules/Card/Card.js
+++ components/modules/Card/Card.js
@@ -4,15 +4,16 @@ import styles from "./Card.module.css";
 
 export default function Card({ title, desc, price, img }) {
   return (
-    <div class="row align-items-center mb-5">
-      <div class="col-4 col-sm-3">
-        <img class={styles.img_menu} src={img} alt="" />
-        <h5 class={`${styles.menu_price}  font_vazir_Medium`}>{price}T</h5>
+    <div className={`row align-items-center mb-5 ${styles.card}`}>
+      <div className="col-4 col-sm-3">
+        <img className={styles.img_menu} src={img} alt={title} />
+        <h5 className={`font_vazir_Medium ${styles.menu_price}`}>{price}T</h5>
       </div>
-      <div class="col-8 col-sm-9  ">
-        <h4 className="font_vazir_Medium">{title}</h4>
-        <p class="m-0  font_vazir_ExtraLight">{desc}</p>
+      <div className="col-8 col-sm-9">
+        <h4 className={`font_vazir_Medium ${styles.menu_title}`}>{title}</h4>
+        <p className={`m-0 font_vazir_ExtraLight ${styles.menu_desc}`}>{desc}</p>
       </div>
     </div>
   );
 }
+
