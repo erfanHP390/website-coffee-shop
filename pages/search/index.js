ba97a6878edede0ca059bfafeff64eae11e3195f
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-30 23:47:11 +0330

    create search-page

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
-export default function Search() {
+import PageHeader from '@/components/modules/PageHeader/PageHeader'
+import Result from '@/components/templates/Search/Result'
+
+export default function Search({data}) {
   return (
-    <h1>
-      search page
-    </h1>
+    <>
+      <PageHeader route={"جستجو ها"} routeLink={"/search"} />
+      <Result  data={data} />
+    </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const { query } = context;
+
+  const res = await fetch("http://localhost:4000/menu");
+  const data = await res.json();
+
+  const searchResult = data.filter(
+    (item) =>
+      item.type.toLowerCase().includes(query.q.toLowerCase()) ||
+      item.title.toLowerCase().includes(query.q.toLowerCase()) ||
+      item.desc.toLowerCase().includes(query.q.toLowerCase())
+  );
+
+
+    return {
+        props: {
+            data: searchResult,
+        }
+    }
+
+
+
 }
