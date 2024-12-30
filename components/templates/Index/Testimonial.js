5a6773511a9ba2b4ab1bca38634cb9500bfe3e38
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-30 10:21:32 +0330

    create template-component testimonial

--- components/templates/Index/Testimonial.js
+++ components/templates/Index/Testimonial.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
-function Testimonial() {
+import TestimonialItem from '@/components/modules/TestimonialItem/TestimonialItem'
+
+import styles from "@/styles/About.module.css"
+
+function Testimonial({data}) {
   return (
-    <div>
-      
+    <div class="container-fluid py-5">
+      <div class="container">
+        <div class="section-title">
+          <h4
+            class={`${styles.text_primary} text-uppercase  font_vazir_Light`}
+            style={{ letterSpacing: "5px" }}
+          >
+            نظرات
+          </h4>
+          <h1 class={` ${styles.display_4}  font_vazir_ExtraBold`}>مشتریان درباره ما چه می گویند</h1>
+        </div>
+        <div class="owl-carousel testimonial-carousel">
+          {data.slice(0, 4).map((comment) => (
+            <TestimonialItem {...comment} key={comment.id} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
