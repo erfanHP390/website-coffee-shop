d85ac533d133a2b4836cff852acbdbb45058e0d4
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-31 11:00:58 +0330

    create page product

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,9 +1,42 @@
 import React from 'react'
 
-export default function SingleProduct() {
+import ProductDetails from '@/components/templates/ProdctDetails/ProductDetails'
+import ProductComment from '@/components/templates/ProductComment/ProductComment'
+
+const Product = ({ product }) => {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <ProductDetails data={product} />
+      <ProductComment />
+    </>
+  );
+};
+
+export async function getStaticPaths(context) {
+  const res = await fetch(`http://localhost:4000/menu`);
+  const products = await res.json();
+
+  const paths = products.map((product) => ({
+    params: { id: String(product.id) },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const res = await fetch(`http://localhost:4000/menu/${params.id}`);
+  const product = await res.json();
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+export default Product;
