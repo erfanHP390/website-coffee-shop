8a470b7652e62efd5ae90124ae8917c27aa89787
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-31 12:46:52 +0330

    update styles and add default 0 for pricing

--- components/templates/ProdctDetails/ProductDetails.js
+++ components/templates/ProdctDetails/ProductDetails.js
@@ -1,51 +1,48 @@
-import React from 'react'
-import styles from "@/styles/Product.module.css"
+import React from "react";
+import styles from "@/styles/Product.module.css";
 
 import { FaRegStar } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa6";
 
 const ProductsDetails = ({ data }) => {
-    return (
-      <div
-        className={`${styles.product_main}   align-items-center mb-5 text-decoration-none justify-content-center`}
-      >
-        <img
-          className={`${styles.product_img}  mb-3 mb-sm-0`}
-          src={data.img}
-          alt=""
-        />
-        <div style={{ width: "500px" }}>
-          <h4 className={`${styles.product_title}  font_vazir_ExtraBold  text-white mb-3`}>{data.title}</h4>
-          <hr style={{ borderColor: "white" }} />
-          <div className={styles.stars}>
-            {new Array(Math.trunc(data.score)).fill().map(() => (
-              <FaStar className={styles.fill_star}  />
-            ))}
-            {new Array(5 - Math.trunc(data.score)).fill().map(() => (
-              <FaRegStar className={styles.empty_star}  />
-            ))}
-          </div>
-          <div className={styles.price_details}>
-            {data.off ? (
-              <>
-                <p>T{data.price - (data.price * data.off) / 100}</p>
-                <p>T{data.price}</p>
-              </>
-            ) : (
-              <p>T{data.price}</p>
-            )}
-          </div>
-          <p style={{ color: "#fff" }} className={`${styles.product_desc}  font_vazir_Medium  m-0 pr-5`}>
-            {data.desc}
-          </p>
-          <button className={styles.shopping_btn}>
-            خرید
-            {/* <FontAwesomeIcon className="ml-2" icon={faShoppingBag} /> */}
-          </button>
-        </div>
-      </div>
-    );
+  const formatPrice = (price) => {
+    const formattedPrice = Math.round(price * 1000).toLocaleString('fa-IR');
+    return formattedPrice;
   };
-  
-  export default ProductsDetails;
-  
+
+  return (
+    <div className={`${styles.product_main} align-items-center mb-5 text-decoration-none justify-content-center`}>
+      <img className={`${styles.product_img} mb-3 mb-sm-0`} src={data.img} alt="" />
+      <div className={styles.product_details}>
+        <h4 className={`${styles.product_title} font_vazir_ExtraBold text-white mb-3`}>{data.title}</h4>
+        <hr className={styles.product_hr} />
+        <div className={styles.stars}>
+          {new Array(Math.trunc(data.score)).fill().map((_, idx) => (
+            <FaStar key={idx} className={styles.fill_star} />
+          ))}
+          {new Array(5 - Math.trunc(data.score)).fill().map((_, idx) => (
+            <FaRegStar key={idx} className={styles.empty_star} />
+          ))}
+        </div>
+        <div className={styles.price_details}>
+          {data.off ? (
+            <>
+              <p className={styles.discount_price}>
+                {formatPrice(data.price - (data.price * data.off) / 100)} تومان
+              </p>
+              <p className={styles.original_price}>{formatPrice(data.price)} تومان</p>
+            </>
+          ) : (
+            <p>{formatPrice(data.price)} تومان</p>
+          )}
+        </div>
+        <p className={`${styles.product_desc} font_vazir_Medium m-0 pr-5`}>{data.desc}</p>
+        <button className={styles.shopping_btn}>
+          خرید
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsDetails;
