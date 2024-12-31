0f32a70843c3f4f66e000cb60f3d5520f898d424
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-31 10:59:56 +0330

    title Link

--- components/modules/Card/Card.js
+++ components/modules/Card/Card.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 import styles from "./Card.module.css";
+import Link from "next/link";
 
-export default function Card({ title, desc, price, img }) {
+export default function Card({id, title, desc, price, img }) {
   return (
     <div className={`row align-items-center mb-5 ${styles.card}`}>
       <div className="col-4 col-sm-3">
@@ -10,7 +11,7 @@ export default function Card({ title, desc, price, img }) {
         <h5 className={`font_vazir_Medium ${styles.menu_price}`}>{price}T</h5>
       </div>
       <div className="col-8 col-sm-9">
-        <h4 className={`font_vazir_Medium ${styles.menu_title}`}>{title}</h4>
+        <Link href={`/product/${id}`} className={`font_vazir_Medium ${styles.menu_title}`}>{title}</Link>
         <p className={`m-0 font_vazir_ExtraLight ${styles.menu_desc}`}>{desc}</p>
       </div>
     </div>
