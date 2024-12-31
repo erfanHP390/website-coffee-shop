21634050af5c22813d5e60aad63a2438a6e71fd7
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-31 22:40:46 +0330

    import btn scroll to top btn width styles

---
+++ components/templates/ScrollTopBtn/ScrollTopBtn.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronsUp } from "react-icons/fi";
+
+import styles from "./ScrollTopBtn.module.css"
+
+export default function ScrollTopBtn() {
+    const [isVisible, setIsVisible] = useState(false);
+    
+    useEffect(() => {
+      const toggleVisibility = () => {
+        if (window.pageYOffset > 0) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      };
+    
+      window.addEventListener('scroll', toggleVisibility);
+      return () => window.removeEventListener('scroll', toggleVisibility);
+    }, []);
+    
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    };
+    
+    return (
+      <button
+        className={`${styles.scroll_to_top}  ${isVisible ? `${styles.visible}` : ''}`}
+        onClick={scrollToTop}
+      >
+        <FiChevronsUp />
+      </button>
+    );
+}
+
+
