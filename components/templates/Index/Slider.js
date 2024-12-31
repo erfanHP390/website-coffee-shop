5a4b46489d6854152954caa4f5039b51276fc4df
erfan hosseini <erfanhosseini768@yahoo.com>
2024-12-31 17:55:00 +0330

    add auto-play to slider

--- components/templates/Index/Slider.js
+++ components/templates/Index/Slider.js
@@ -7,82 +7,86 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/autoplay";
 
 // import required modules
-import { Navigation } from "swiper/modules";
+import { Navigation, Autoplay } from "swiper/modules";
 
 function Slider() {
   return (
     <Swiper
-      // rewind={true}
       loop={true}
       navigation={true}
-      modules={[Navigation]}
+      autoplay={{
+        delay: 10000, // مدت زمان به میلی‌ثانیه، ۱۰ ثانیه
+        disableOnInteraction: false,
+      }}
+      modules={[Navigation, Autoplay]}
       className={styles.swiper}
     >
-<SwiperSlide className={styles.swiper_slide}>
-  <video 
-    autoPlay 
-    muted 
-    loop 
-    id="bg-video" 
-    className={styles.video_background}
-  >
-    <source src="/images/film/film-1.mp4" type="video/mp4" />
-  </video>
-  <div className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
-    <h2 className={`text-primary font-weight-medium m-0  font_vazir_Medium  ${styles.h2_heading}`}>
-      برای شما بهترین ها را به ارمغان آوردیم
-    </h2>
-    <h1 className="display-1 text-white m-0  font_vazir_Medium ">در کنار شماییم</h1>
-    <h2 className="text-white m-0  font_vazir_Medium ">* از سال 1395 *</h2>
-  </div>
-</SwiperSlide>
-<SwiperSlide className={styles.swiper_slide}>
-  <video 
-    autoPlay 
-    muted 
-    loop 
-    id="bg-video" 
-    className={styles.video_background}
-  >
-    <source src="/images/film/film-5.mp4" type="video/mp4" />
-  </video>
-  <div className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
-    <h2 className={`text-primary font-weight-medium m-0  font_vazir_Medium  ${styles.h2_heading}`}>
-      بهترین قهوه از دل جنگل های اتیوپی
-    </h2>
-    <h1 className="display-1 text-white m-0 font_vazir_Medium">از آفریقا تا ایران</h1>
-  </div>
-</SwiperSlide>
-<SwiperSlide className={styles.swiper_slide}>
-  <video 
-    autoPlay 
-    muted 
-    loop 
-    id="bg-video" 
-    className={styles.video_background}
-  >
-    <source src="/images/film/film-2.mp4" type="video/mp4" />
-  </video>
-  <div className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
-    <h2 className={`text-primary font-weight-medium m-0  font_vazir_Medium  ${styles.h2_heading}`}>
-      8 سال بهترین در مشتری مداری
-    </h2>
-    <h1 className="display-1 text-white m-0 font_vazir_Medium">در کنار خاطرات شما</h1>
-  </div>
-</SwiperSlide>
-<SwiperSlide
+      <SwiperSlide className={styles.swiper_slide}>
+        <video 
+          autoPlay 
+          muted 
+          loop 
+          id="bg-video" 
+          className={styles.video_background}
+        >
+          <source src="/images/film/film-1.mp4" type="video/mp4" />
+        </video>
+        <div className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
+          <h2 className={`text-primary font-weight-medium m-0  font_vazir_Medium  ${styles.h2_heading}`}>
+            برای شما بهترین ها را به ارمغان آوردیم
+          </h2>
+          <h1 className="display-1 text-white m-0  font_vazir_Medium ">در کنار شماییم</h1>
+          <h2 className="text-white m-0  font_vazir_Medium ">* از سال 1395 *</h2>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiper_slide}>
+        <video 
+          autoPlay 
+          muted 
+          loop 
+          id="bg-video" 
+          className={styles.video_background}
+        >
+          <source src="/images/film/film-5.mp4" type="video/mp4" />
+        </video>
+        <div className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
+          <h2 className={`text-primary font-weight-medium m-0  font_vazir_Medium  ${styles.h2_heading}`}>
+            بهترین قهوه از دل جنگل های اتیوپی
+          </h2>
+          <h1 className="display-1 text-white m-0 font_vazir_Medium">از آفریقا تا ایران</h1>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiper_slide}>
+        <video 
+          autoPlay 
+          muted 
+          loop 
+          id="bg-video" 
+          className={styles.video_background}
+        >
+          <source src="/images/film/film-2.mp4" type="video/mp4" />
+        </video>
+        <div className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
+          <h2 className={`text-primary font-weight-medium m-0  font_vazir_Medium  ${styles.h2_heading}`}>
+            8 سال بهترین در مشتری مداری
+          </h2>
+          <h1 className="display-1 text-white m-0 font_vazir_Medium">در کنار خاطرات شما</h1>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide
         className={styles.swiper_slide}
         style={{ backgroundImage: 'url("/images/slider/slider-3.jpg")' }}
       >
         <div
-          class={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}
+          className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}
         >
           <h2 className={`text-primary font-weight-medium m-0  font_vazir_Medium  ${styles.h2_heading}`}>
             محیطی آرامش بخش
           </h2>
-          <h1 class="display-1 text-white m-0 font_vazir_Medium">در کافه آرامش</h1>
+          <h1 className="display-1 text-white m-0 font_vazir_Medium">در کافه آرامش</h1>
         </div>
       </SwiperSlide>
     </Swiper>
