77d2ec3b92bf99c7f9ffb844daf8710d6ac07577
erfan hosseini <erfanhosseini768@yahoo.com>
2025-01-02 18:46:59 +0330

    change address api

--- pages/search/index.js
+++ pages/search/index.js
@@ -21,7 +21,7 @@ export default function Search({ data }) {
 export async function getServerSideProps(context) {
   const { query } = context;
 
-  const res = await fetch("http://localhost:4000/menu");
+  const res = await fetch("http://localhost:3000/api/menu");
   const data = await res.json();
 
   const searchResult = data.filter(
