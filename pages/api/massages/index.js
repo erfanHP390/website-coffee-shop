c45029b8953d1a2578f8a548665037b08e494a2e
erfan hosseini <erfanhosseini768@yahoo.com>
2025-01-02 17:58:46 +0330

    create api for contact-us-form

--- pages/api/massages/index.js
+++ pages/api/massages/index.js
@@ -1,7 +1,57 @@
-import React from 'react'
+import React from "react";
+import fs from "fs";
+import path from "path";
 
-function index(req , res) {
-  return res.json({massage: "massages"})
+function index(req, res) {
+  switch (req.method) {
+    case "GET": {
+      const dbPath = path.join(process.cwd(), "data", "db.json");
+      const data = fs.readFileSync(dbPath);
+      const parsedData = JSON.parse(data);
+
+      res.json(parsedData.massages);
+      break;
+    }
+
+    case "POST": {
+      const { name, email, subject, body } = req.body;
+
+      const dbPath = path.join(process.cwd(), "data", "db.json");
+      const data = fs.readFileSync(dbPath);
+      const parsedData = JSON.parse(data);
+
+      parsedData.massages.push({
+        id: crypto.randomUUID(),
+        name,
+        email,
+        subject,
+        body,
+      });
+
+      const err = fs.writeFileSync(dbPath, JSON.stringify(parsedData));
+
+      if (err) {
+      } else {
+        res
+          .status(201)
+          .json({ massage: "massages  is successfully added" });
+      }
+
+      break;
+    }
+
+    case "PUT": {
+      return res.json({ massage: "PUT services" });
+      break;
+    }
+    case "DELETE": {
+      return res.json({ massage: "DELETE Massages" });
+      break;
+    }
+    default: {
+      return res.json({ massage: "default Massages" });
+    }
+  }
 }
 
-export default index
+export default index;
