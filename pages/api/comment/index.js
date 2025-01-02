6ed2ddc27b044a02d3a226f2b9beec8fc8cfb9e1
erfan hosseini <erfanhosseini768@yahoo.com>
2025-01-02 09:41:09 +0330

    add server.js

--- pages/api/comment/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function index(req , res) {
-  return res.json({massage: "comment"})
-}
-
-export default index
--- pages/api/newsLetter/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function index(req , res) {
-  return res.json({massage: "news-letter"})
-}
-
-export default index
