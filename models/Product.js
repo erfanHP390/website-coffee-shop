1bd54fe19a868ad23314471ed00d1c1cb394d679
erfan hosseini <erfanhosseini768@yahoo.com>
2025-09-14 21:15:19 +0330

    create model products

---
+++ models/Product.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    off: {
+      type: String,
+      required: true,
+    },
+    score: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const model = mongoose.models.Product || mongoose.model("Product", schema);
+export default model;
