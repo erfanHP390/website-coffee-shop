5e041554c6a1b0af2bbc56ab04932af29e2c4246
erfan hosseini <erfanhosseini768@yahoo.com>
2025-09-14 22:42:54 +0330

    create page for product-details

---
+++ src/app/product/[id]/page.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import "@/styles/Product.module.css";
+import ProductModel from "@/models/Product"
+import  styles from "@/styles/Product.module.css";
+
+
+import ProductsDetails from "@/components/templates/ProdctDetails/ProductDetails";
+import connectToDB from '@/configs/db';
+
+const Product = async ({ params }) => {
+  connectToDB()
+  const product = await ProductModel.findOne({ _id: params.id }).lean()
+  
+  
+  return (
+    <>
+      <Head>
+        {" "}
+        <title>{product.title}</title>{" "}
+        <link rel="icon" type="image/png" href="/images/service-2.jpg" />{" "}
+      </Head>
+      <ProductsDetails product={JSON.parse(JSON.stringify(product))}  />
+      {/* <ProductComment data={comments} /> */}
+    </>
+  );
+};
+
+
+export default Product;
