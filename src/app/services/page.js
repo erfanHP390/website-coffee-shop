49f089085eb3f4b40c98ea3d3670dc181fe7f80d
erfan hosseini <erfanhosseini768@yahoo.com>
2025-09-14 22:43:45 +0330

    remove static-props

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import fs from "fs"
-import path from "path"
+
 
 import PageHeader from "@/components/modules/PageHeader/PageHeader";
 import Services from "@/components/templates/Index/Services";
@@ -15,22 +14,7 @@ export default function ServicesPage({ data }) {
         <link rel="icon" type="image/png" href="/images/service-2.jpg" />{" "}
       </Head>
       <PageHeader route={"خدمات"} routeLink={"services"} />
-      <Services services={data.services} />
+      <Services  />
     </>
   );
-}
-
-export async function getStaticProps() {
-  const dbPath = path.join(process.cwd(), "data", "db.json");
-  const data = fs.readFileSync(dbPath);
-  const parsedData = JSON.parse(data);
-
-  return {
-    props: {
-      data: {
-        services: parsedData.services,
-      },
-    },
-    revalidate: 60 * 60 * 12,
-  };
 }
