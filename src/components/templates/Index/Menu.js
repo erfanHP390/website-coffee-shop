460f667f24af5666d08a14a4b7b11673bc309697
erfan hosseini <erfanhosseini768@yahoo.com>
2025-09-14 22:44:32 +0330

    get product-infos from parent

--- src/components/templates/Index/Menu.js
+++ src/components/templates/Index/Menu.js
@@ -1,45 +1,45 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../styles/About.module.css";
+import Card from "@/components/modules/Card/Card";
 
-import styles from "../../../styles/About.module.css"
-import Card from '@/components/modules/Card/Card'
+function Menu({ products }) {
 
+  const hotDrinks = products.filter((item) => item.type === "hot").slice(0, 3);
+  const coldDrinks = products.filter((item) => item.type === "cold").slice(0, 3);
 
-function Menu({data}) {
   return (
-    <div className="container-fluid pt-5" >
+    <div className="container-fluid pt-5">
       <div className="container">
         <div className="section-title">
           <h4
-            className={`${styles.text_primary}  font_vazir_Light  ${styles.text_uppercase}`}
+            className={`${styles.text_primary} font_vazir_Light ${styles.text_uppercase}`}
             style={{ letterSpacing: "5px" }}
           >
             منوی ما
           </h4>
-          <h1 className={`${styles.display_4} font_vazir_ExtraBold`}>با تنوعی از نوشیدنی ها</h1>
+          <h1 className={`${styles.display_4} font_vazir_ExtraBold`}>
+            با تنوعی از نوشیدنی ها
+          </h1>
         </div>
+
         <div className="row">
           <div className="col-lg-6">
-            <h1 className="mb-5  font_vazir_Bold">نوشیدنی داغ</h1>
-            {
-              data.filter((item) => item.type === "hot").slice(0,3).map((item) => (
-                <Card {...item}  key={item.id} />
-              ))
-            }
-
+            <h1 className="mb-5 font_vazir_Bold">نوشیدنی داغ</h1>
+            {hotDrinks.map((item) => (
+              <Card title={item.title} description={item.description} img={item.img}  price={item.price} _id={item._id} key={item._id} />
+            ))}
           </div>
 
           <div className="col-lg-6">
-            <h1 className="mb-5   font_vazir_Bold">نوشیدنی سرد</h1>
-            {
-              data.filter((item) => item.type === "cold").slice(0,3).map((item) => (
-                <Card {...item} key={item.id} />
-              ))
-            }
+            <h1 className="mb-5 font_vazir_Bold">نوشیدنی سرد</h1>
+            {coldDrinks.map((item) => (
+              <Card title={item.title} description={item.description} img={item.img}  price={item.price} _id={item._id} key={item._id} />
+            ))}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
