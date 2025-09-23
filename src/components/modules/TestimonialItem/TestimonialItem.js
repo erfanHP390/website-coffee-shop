e2cec5ac2531d680b2db3aaeb98de6c88be60b24
erfan hosseini <erfanhosseini768@yahoo.com>
2025-09-23 19:16:50 +0330

    update comment-details

--- src/components/modules/TestimonialItem/TestimonialItem.js
+++ src/components/modules/TestimonialItem/TestimonialItem.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import styles from './TestimonialItem.module.css';
 
-function TestimonialItem({ id, name, comment, rating, profile }) {
+function TestimonialItem({ _id , username , email , score , content }) {
 
 
-  const validRating = typeof rating === 'number' && !isNaN(rating) ? Math.min(Math.max(rating, 0), 5) : 0;
+  const validRating = typeof score === 'number' && !isNaN(score) ? Math.min(Math.max(score, 0), 5) : 0;
 
   return (
     <div className={styles.testimonialItem}>
       <div className="d-flex align-items-center mb-3">
-        <img className={styles.profileImage} src={profile} alt={`${name}'s profile`} />
+        <img className={styles.profileImage} src={""} alt={`${username}'s profile`} />
         <div className="ml-3">
-          <h4 className={`${styles.name} font_vazir_Medium `}>{name}</h4>
+          <h4 className={`${styles.name} font_vazir_Medium `}>{username}</h4>
         </div>
       </div>
-      <p className={`${styles.comment} font_vazir_Regular `}>{comment}</p>
+      <p className={`${styles.comment} font_vazir_Regular `}>{content}</p>
       <div className={styles.rating}>
         {Array(validRating).fill().map((_, i) => (
           <span key={i} className={styles.star}>&#9733;</span>
