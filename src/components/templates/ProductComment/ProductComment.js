4dd95204b68c70c12248f9fc59223f5c53055ac3
erfan hosseini <erfanhosseini768@yahoo.com>
2025-09-23 19:16:37 +0330

    get comments from db to show

--- src/components/templates/ProductComment/ProductComment.js
+++ src/components/templates/ProductComment/ProductComment.js
@@ -1,32 +1,35 @@
-import styles from '@/styles/Product.module.css'
-import React from 'react';
+"use client";
+import styles from "@/styles/Product.module.css";
+import React from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
 // import required modules
-import { Pagination, Autoplay } from 'swiper/modules';
-import TestimonialItem from '@/components/modules/TestimonialItem/TestimonialItem'
+import { Pagination, Autoplay } from "swiper/modules";
+import TestimonialItem from "@/components/modules/TestimonialItem/TestimonialItem";
 
-export default function ProductComment({data}) {
+export default function ProductComment({ product }) {
   return (
     <div className="container-fluid pt-5">
       <div className="container">
-      <div className="section-title">
+        <div className="section-title">
           <h4
             className={`${styles.text_primary} font_vazir_Light ${styles.text_uppercase}`}
             style={{ letterSpacing: "5px" }}
           >
-             نظرات شما
+            نظرات شما
           </h4>
-          <h1 className={`${styles.display_4} font_vazir_ExtraBold`}>تجربیات شما از این محصول</h1>
+          <h1 className={`${styles.display_4} font_vazir_ExtraBold`}>
+            تجربیات شما از این محصول
+          </h1>
         </div>
         <div className="row">
-          {data && data.length > 0 ? (
+          {product.comments && product.comments.length > 0 ? (
             <Swiper
               loop={true}
               slidesPerView={1}
@@ -41,11 +44,17 @@ export default function ProductComment({data}) {
               modules={[Pagination, Autoplay]}
               className="mySwiper"
             >
-              {data.map((comment) => (
-                <SwiperSlide className={styles.swiper_slide} key={comment.id}>
-                  <TestimonialItem key={comment.id} {...comment} />
-                </SwiperSlide>
-              ))}
+              {product.comments.map(
+                (comment) =>
+                  comment.isAccept && (
+                    <SwiperSlide
+                      className={styles.swiper_slide}
+                      key={comment._id}
+                    >
+                      <TestimonialItem key={comment._id} {...comment} />
+                    </SwiperSlide>
+                  )
+              )}
             </Swiper>
           ) : (
             <div className={styles.no_comments}>
@@ -55,5 +64,5 @@ export default function ProductComment({data}) {
         </div>
       </div>
     </div>
-  )
+  );
 }
