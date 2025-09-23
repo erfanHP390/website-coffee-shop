f56961f564c16e621f74a43254fd0bd0bd7c2367
erfan hosseini <erfanhosseini768@yahoo.com>
2025-09-23 19:16:25 +0330

    get products and their comments from db

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -6,10 +6,12 @@ import  styles from "@/styles/Product.module.css";
 
 import ProductsDetails from "@/components/templates/ProdctDetails/ProductDetails";
 import connectToDB from '@/configs/db';
+import CommentForm from '@/components/templates/ProductComment/commentForm/CommentForm';
+import ProductComment from '@/components/templates/ProductComment/ProductComment';
 
 const Product = async ({ params }) => {
   connectToDB()
-  const product = await ProductModel.findOne({ _id: params.id }).lean()
+  const product = await ProductModel.findOne({ _id: params.id }).populate("comments").lean()
   
   
   return (
@@ -20,7 +22,8 @@ const Product = async ({ params }) => {
         <link rel="icon" type="image/png" href="/images/service-2.jpg" />{" "}
       </Head>
       <ProductsDetails product={JSON.parse(JSON.stringify(product))}  />
-      {/* <ProductComment data={comments} /> */}
+      <ProductComment product={JSON.parse(JSON.stringify(product))} />
+      <CommentForm  productId={product._id} />
     </>
   );
 };
