180276977264b9b8ea686de92cc4cce92ecb80f7
erfan hosseini <erfanhosseini768@yahoo.com>
2025-09-28 22:03:57 +0330

    import products from database

--- src/components/templates/menu/MenuContent.js
+++ src/components/templates/menu/MenuContent.js
@@ -5,33 +5,33 @@ import React, { useState } from "react";
 import PageHeader from "@/components/modules/PageHeader/PageHeader";
 import styles from "@/styles/About.module.css";
 import Card from "@/components/modules/Card/Card";
-function MenuContent() {
-  //       const [currentPage, setCurrentPage] = useState(1);
-  //   const hotDrinksPerPage = 3;
-  //   const coldDrinksPerPage = 3;
+function MenuContent({products}) {
+        const [currentPage, setCurrentPage] = useState(1);
+    const hotDrinksPerPage = 3;
+    const coldDrinksPerPage = 3;
 
-  //   const handlePagination = (pageNumber) => {
-  //     setCurrentPage(pageNumber);
-  //   };
+    const handlePagination = (pageNumber) => {
+      setCurrentPage(pageNumber);
+    };
 
-  //   const hotDrinks = data.menu.filter((item) => item.type === "hot");
-  //   const coldDrinks = data.menu.filter((item) => item.type === "cold");
+    const hotDrinks = products.filter((item) => item.type === "hot");
+    const coldDrinks = products.filter((item) => item.type === "cold");
 
-  //   const totalPages = Math.ceil(
-  //     Math.max(
-  //       hotDrinks.length / hotDrinksPerPage,
-  //       coldDrinks.length / coldDrinksPerPage
-  //     )
-  //   );
+    const totalPages = Math.ceil(
+      Math.max(
+        hotDrinks.length / hotDrinksPerPage,
+        coldDrinks.length / coldDrinksPerPage
+      )
+    );
 
-  //   const currentHotDrinks = hotDrinks.slice(
-  //     (currentPage - 1) * hotDrinksPerPage,
-  //     currentPage * hotDrinksPerPage
-  //   );
-  //   const currentColdDrinks = coldDrinks.slice(
-  //     (currentPage - 1) * coldDrinksPerPage,
-  //     currentPage * coldDrinksPerPage
-  //   );
+    const currentHotDrinks = hotDrinks.slice(
+      (currentPage - 1) * hotDrinksPerPage,
+      currentPage * hotDrinksPerPage
+    );
+    const currentColdDrinks = coldDrinks.slice(
+      (currentPage - 1) * coldDrinksPerPage,
+      currentPage * coldDrinksPerPage
+    );
 
   return (
     <>
@@ -52,15 +52,15 @@ function MenuContent() {
           <div className="row">
             <div className="col-lg-6">
               <h1 className="mb-5 font_vazir_Bold">نوشیدنی داغ</h1>
-              {/* {currentHotDrinks.map((item) => (
+              {currentHotDrinks.map((item) => (
                 <Card key={item.id} {...item} />
-              ))} */}
+              ))}
             </div>
             <div className="col-lg-6">
               <h1 className="mb-5 font_vazir_Bold">نوشیدنی سرد</h1>
-              {/* {currentColdDrinks.map((item) => (
+              {currentColdDrinks.map((item) => (
                 <Card key={item.id} {...item} />
-              ))} */}
+              ))}
             </div>
           </div>
           <div className={`${styles.pagination}`}>
